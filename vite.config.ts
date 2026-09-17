31a0b1f6efbbca4f32752f6bbd32c6b6bab7b310
dadaz <keshengtiquan@163.com>
2026-09-17 11:25:58 +0800

    添加vite-plugin-vue-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "node:url";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 export default () => {
   return defineConfig({
@@ -13,6 +14,6 @@ export default () => {
         "@": fileURLToPath(new URL("./src", import.meta.url))
       }
     },
-    plugins: [vue(), tailwindcss()]
+    plugins: [vue(), tailwindcss(), vueDevTools()]
   });
 };
